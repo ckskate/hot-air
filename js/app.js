df1668a7f7862606c680b61a41ec399d5a7d7643
Connor Killion <amations@gmail.com>
2018-11-24 17:27:28 -0600

    potentially finished

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 // simulation parameters
 var totalRecordingTime = 3000; // ms
 var dataSampleInterval = 250; // ms
-var totalNumberOfSamples = totalRecordingTime / dataSampleInterval;
+var totalNumberOfSamples = (totalRecordingTime / dataSampleInterval) - 1;
 var analyserFftSize = 2048; // I just pulled this from a mozilla example
 var iterations = 0;
 // expose the following two parameters to the user
@@ -28,15 +28,21 @@ analyser.maxDecibels = -10;
 analyser.smoothingTimeConstant = 0.85;
 
 var audioData = [];
+var untouchedAudioData = [];
 var dataCollectionInterval = null;
 
 // recording
-var record = document.querySelector('.record');
+var recordButton = document.querySelector('.record');
+var playButton = document.querySelector('.play');
+var resetButton = document.querySelector('.reset');
+var saveButton = document.querySelector('.save');
 // var player = document.getElementById('player');
 var totalTime = 0.0;
 var chunks = [];
 var timerInterval = null;
-var mediaRecorder;
+
+// downloader
+var capturer = new CCapture( { format: 'gif', workersPath: '/js' } );
 
 drawDefaultLine();
 
@@ -74,14 +80,20 @@ function drawDefaultLine() {
 }
 
 // function for starting the recording when record button is pressed
-record.onclick = function() {
+recordButton.onclick = function() {
   if (timerInterval !== null) {
     console.log("already recording");
     return;
   }
 
+  resetButton.classList.add("disabled");
+  playButton.classList.add("disabled");
+  saveButton.classList.add("disabled");
+  recordButton.classList.add("disabled");
+
   // clear the recorded data
   audioData = [];
+  untouchedAudioData = [];
   // clear the canvas
   canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
   // record for 3 seconds
@@ -90,6 +102,57 @@ record.onclick = function() {
   dataCollectionInterval = setInterval(collectData, dataSampleInterval);
 };
 
+playButton.onclick = function() {
+  if (audioData !== [] && audioData !== null
+    && audioData !== undefined) {
+    capturer.start();
+    coolAndUpdateGraph();
+    playButton.classList.add("disabled");
+  }
+}
+
+resetButton.onclick = function() {
+  if (untouchedAudioData === [] || untouchedAudioData === null
+    || untouchedAudioData === undefined) {
+    return;
+  }
+
+  var WIDTH = canvas.width;
+  var HEIGHT = canvas.height;
+
+  canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+
+  canvasCtx.lineWidth = 2;
+  canvasCtx.strokeStyle = 'rgb(5, 56, 107)';
+
+  canvasCtx.beginPath();
+
+  var sliceWidth = WIDTH * 1.0 / untouchedAudioData.length;
+  var x = 0.5;
+
+  for (var i = 0; i < untouchedAudioData.length; i++) {
+    var v = untouchedAudioData[i] / 128.0;
+    var y = v * HEIGHT/2;
+
+    if (i === 0) {
+      canvasCtx.moveTo(x, y + 0.5);
+    } else {
+      canvasCtx.lineTo(x, y + 0.5);
+    }
+    x += sliceWidth;
+  }
+
+  canvasCtx.lineTo(canvas.width, canvas.height/2 + 0.5);
+  canvasCtx.stroke();
+
+  resetButton.classList.add("disabled");
+  playButton.classList.remove("disabled");
+}
+
+saveButton.onclick = function() {
+  console.log("saving");
+  capturer.save();
+}
 
 // function for stopping the recording
 function stopRecording() {
@@ -99,9 +162,13 @@ function stopRecording() {
   dataCollectionInterval = null;
 
   // TODO: remove the .disabled classes from the buttons
+  playButton.classList.remove("disabled");
+  saveButton.classList.remove("disabled");
+  recordButton.classList.remove("disabled");
 
   iterations = 0;
-  coolAndUpdateGraph(); // update and draw new waveform
+  untouchedAudioData = audioData.slice(); // make a copy of the audio data
+  // coolAndUpdateGraph(); // update and draw new waveform
 }
 
 function collectData() {
@@ -152,6 +219,10 @@ function coolAndUpdateGraph() {
   iterations++;
   if (iterations <= iterationCount) {
     requestAnimationFrame(coolAndUpdateGraph);
+  } else {
+    console.log("stopping the capture");
+    capturer.stop();
+    resetButton.classList.remove("disabled");
   }
 
   let n = audioData.length;
@@ -196,4 +267,5 @@ function coolAndUpdateGraph() {
 
   canvasCtx.lineTo(canvas.width, canvas.height/2 + 0.5);
   canvasCtx.stroke();
+  capturer.capture(canvas);
 }
